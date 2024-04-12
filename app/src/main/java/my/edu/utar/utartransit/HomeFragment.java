b32136e719c00cfe787daa4b984edb5fa02f80c6
WongJennJia <jennjiawong@gmail.com>
2024-04-12 21:37:33 +0800

    Test
    test

--- app/src/main/java/my/edu/utar/utartransit/HomeFragment.java
+++ app/src/main/java/my/edu/utar/utartransit/HomeFragment.java
@@ -1,18 +1,20 @@
 package my.edu.utar.utartransit;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+
     }
 }
