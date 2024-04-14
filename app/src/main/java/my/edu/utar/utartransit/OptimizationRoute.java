c8cfa5af90eac56964111c8a59455c1865a7fd19
Connie TMX <leo1111xx@gmail.com>
2024-04-15 00:29:48 +0800

    add back the google map sdk and the layout

--- app/src/main/java/my/edu/utar/utartransit/OptimizationRoute.java
+++ app/src/main/java/my/edu/utar/utartransit/OptimizationRoute.java
@@ -62,7 +62,14 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class OptimizationRoute extends AppCompatActivity {
+public class OptimizationRoute extends AppCompatActivity implements OnMapReadyCallback {
+
+    // Google Map
+    private final int FINE_PERMISSION_CODE = 1;
+    private GoogleMap mMap;
+    private SearchView mapSearchView;
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     // Filtter by
     private ToggleButton toggleFilterBy;
@@ -95,6 +102,39 @@ public class OptimizationRoute extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_optimization_route);
+
+        // Google Map
+        mapSearchView = findViewById(R.id.mapSearch);
+        mapSearchView.setQueryHint("Search...");
+
+        mapSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = mapSearchView.getQuery().toString();
+                List<Address> addressList = null;
+                if (location != null){
+                    Geocoder geocoder = new Geocoder(OptimizationRoute.this);
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
 
         // Filtter by
         toggleFilterBy = findViewById(R.id.toggleFilterBy);
@@ -187,6 +227,52 @@ public class OptimizationRoute extends AppCompatActivity {
                 spinnerArrival.setSelection(departureAdapter.getPosition(selectedDeparture));
             }
         });
+    }
+
+    // Google Map
+    private void getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_PERMISSION_CODE);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null){
+                    currentLocation = location;
+
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    mapFragment.getMapAsync(OptimizationRoute.this);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        mMap = googleMap;
+        LatLng location = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(location).title("My Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 13));
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+        //mMap.getUiSettings().setScrollGesturesEnabled(true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == FINE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLastLocation();
+            }else {
+                Toast.makeText(this, "Location permission is denied, please allow the permission.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     //supabase
